ff900e9727f2f31ff55ec9d05a4feeebcb207d95
Paw565pl <paw565pl@gmail.com>
2024-03-11 12:17:31 +0100

    lab01: add cesar find key function

--- lab01/cesar.py
+++ lab01/cesar.py
@@ -64,7 +64,44 @@ def decrypt():
 
 
 def find_key():
-    pass
+    with open("./data/crypto.txt", "r") as file:
+        encrypted_text = file.read()
+
+    with open("./data/extra.txt", "r") as file:
+        helper_plain_text = file.read()
+
+    try:
+        first_letter_of_encrypted_text = encrypted_text[0]
+        first_letter_of_helper_plain_text = helper_plain_text[0]
+    except IndexError:
+        print("files are empty!")
+        exit(1)
+
+    key = None
+    for potential_key in range(1, 26):
+        encrypted_first_letter_of_helper_plain_text = _encrypt_char(
+            first_letter_of_helper_plain_text, potential_key
+        )
+        if (
+            encrypted_first_letter_of_helper_plain_text
+            == first_letter_of_encrypted_text
+        ):
+            key = potential_key
+            break
+
+    if key is None:
+        print("unable to find key!")
+        exit(1)
+
+    with open("./data/key-new.txt", "w") as file:
+        file.write(str(key))
+
+    decrypted_text = ""
+    for char in encrypted_text:
+        decrypted_text += _decrypt_char(char, key)
+
+    with open("./data/decrypt.txt", "w") as file:
+        file.write(decrypted_text)
 
 
 def break_code():
@@ -72,7 +109,6 @@ def break_code():
         encrypted_text = file.read()
 
     output = []
-
     for key in range(1, 26):
         decrypted_text = ""
         for char in encrypted_text:

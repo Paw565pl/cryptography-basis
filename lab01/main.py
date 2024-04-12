58b3d26a27ef0d05b84c710e4fc2863b2b44e7b4
Paw565pl <paw565pl@gmail.com>
2024-04-12 21:56:11 +0200

    lab01: refactor code to use argparse

--- lab01/main.py
+++ lab01/main.py
@@ -1,36 +1,46 @@
+from argparse import ArgumentParser
 from sys import argv
 
 import affine
 import cesar
 
-try:
-    arg1 = argv[1]
-    arg2 = argv[2]
-except IndexError:
-    print("please provide two arguments")
-    exit(1)
 
-first_arg_options = ["-c", "-a"]
-second_arg_options = ["-e", "-d", "-j", "-k"]
+def main():
+    parser = ArgumentParser(description="cesar and affine cypher implementation")
 
-if arg1 not in first_arg_options or arg2 not in second_arg_options:
-    print("invalid arguments")
-    exit(1)
+    cypher_group = parser.add_mutually_exclusive_group(required=True)
+    cypher_group.add_argument("-c", help="cesar cipher", action="store_true")
+    cypher_group.add_argument("-a", help="affine cipher", action="store_true")
 
-args_to_function = {
-    "-c": {
-        "-e": cesar.encrypt,
-        "-d": cesar.decrypt,
-        "-j": cesar.find_key,
-        "-k": cesar.break_code,
-    },
-    "-a": {
-        "-e": affine.encrypt,
-        "-d": affine.decrypt,
-        "-j": affine.find_key,
-        "-k": affine.break_code,
-    },
-}
+    operation_group = parser.add_mutually_exclusive_group(required=True)
+    operation_group.add_argument("-e", help="encrypt", action="store_true")
+    operation_group.add_argument("-d", help="decrypt", action="store_true")
+    operation_group.add_argument("-j", help="find key", action="store_true")
+    operation_group.add_argument("-k", help="break code", action="store_true")
 
-args_to_function[arg1][arg2]()
-print("done!")
+    parser.parse_args()
+
+    cypher_argument = argv[1]
+    operation_argument = argv[2]
+
+    args_to_function = {
+        "-c": {
+            "-e": cesar.encrypt,
+            "-d": cesar.decrypt,
+            "-j": cesar.find_key,
+            "-k": cesar.break_code,
+        },
+        "-a": {
+            "-e": affine.encrypt,
+            "-d": affine.decrypt,
+            "-j": affine.find_key,
+            "-k": affine.break_code,
+        },
+    }
+
+    args_to_function[cypher_argument][operation_argument]()
+    print("done!")
+
+
+if __name__ == "__main__":
+    main()
